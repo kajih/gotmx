c48f9cf60ef4c00f7414bb5e095af853665be35e
Johan Häggqvist <johan.haggqvist@dataductus.se>
2025-01-08 11:49:24 +0100

    Tailwinding

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
@@ -12,18 +14,44 @@ import (
 //go:embed embed
 var embeddedFiles embed.FS
 
+type Template struct {
+	tmpl *template.Template
+}
+
+func newTemplate() *Template {
+	return &Template{
+		tmpl: template.Must(template.ParseFS(embeddedFiles, "embed/templates/*.html")),
+	}
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
+}
+
+type Count struct {
+	Count int
+}
+
 func main() {
 	e := echo.New()
 
+	e.Use(middleware.Logger())
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
 		HTML5:      true,
 		Root:       "/embed/web", // because files are located in `web` directory in `embed` fs
 		Filesystem: http.FS(embeddedFiles),
 	}))
 
-	api := e.Group("/api")
-	api.GET("/users", func(c echo.Context) error {
-		return c.String(http.StatusOK, "users")
+	count := Count{Count: 0}
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "blocks-index", count)
+	})
+
+	e.GET("/getcount", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "countBlock", count)
 	})
 
 	if err := e.Start(":8080"); err != nil && !errors.Is(err, http.ErrServerClosed) {
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./cmd/embed/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
